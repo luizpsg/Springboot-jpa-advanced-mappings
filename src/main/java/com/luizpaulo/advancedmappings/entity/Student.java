24753ae461d076bb1864aad770358f709b46d861
luizpsg <luiz.saud@sga.pucminas.br>
2024-04-24 18:01:35 -0300

    Add association between Student and Course entities

--- src/main/java/com/luizpaulo/advancedmappings/entity/Student.java
+++ src/main/java/com/luizpaulo/advancedmappings/entity/Student.java
@@ -1,5 +1,8 @@
 package com.luizpaulo.advancedmappings.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -18,6 +21,16 @@ public class Student {
 
   @Column(name = "email")
   private String email;
+
+  @ManyToMany(
+    fetch = FetchType.LAZY, 
+    cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(
+    name = "course_student",
+    joinColumns = @JoinColumn(name = "student_id"),
+    inverseJoinColumns = @JoinColumn(name = "course_id")
+  )
+  private List<Course> courses;
 
   public Student() {
   }
@@ -58,6 +71,23 @@ public class Student {
 
   public void setEmail(String email) {
     this.email = email;
+  }
+
+  public List<Course> getCourses() {
+    return this.courses;
+  }
+
+  public void setCourses(List<Course> courses) {
+    this.courses = courses;
+  }
+
+  // add a convenience method
+  public void addCourse(Course theCourse) {
+    if (courses == null) {
+      courses = new ArrayList<>();
+    }
+
+    courses.add(theCourse);
   }
 
   @Override
