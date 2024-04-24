ba30492a196a884e4e3e46a6b62a910d54a8b702
luizpsg <luiz.saud@sga.pucminas.br>
2024-04-24 00:02:35 -0300

    Add Instructor entity class

---
+++ src/main/java/com/luizpaulo/advancedmappings/entity/Instructor.java
@@ -0,0 +1,96 @@
+package com.luizpaulo.advancedmappings.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+// annotate the class as an entity and map to db table
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+  // define the fields
+  // annotate the fields with db column names
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "email")
+  private String email;
+
+  //** set up mapping to InstructorDetail entity
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "instructor_detail_id")
+  private InstructorDetail instructorDetail;
+
+  // create constructors
+  public Instructor() {
+  }
+
+  public Instructor(String firstName, String lastName, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  // generate getter/setter methods
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public InstructorDetail getInstructorDetail() {
+    return this.instructorDetail;
+  }
+
+  public void setInstructorDetail(InstructorDetail instructorDetail) {
+    this.instructorDetail = instructorDetail;
+  }
+
+  // generate toString() method
+  @Override
+  public String toString() {
+    return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+        + ", instructorDetail=" + instructorDetail + "]";
+  }
+}
