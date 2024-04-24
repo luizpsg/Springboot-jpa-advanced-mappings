1fef0ac8319da3e71d61aa1b870ff45930a6f45c
luizpsg <luiz.saud@sga.pucminas.br>
2024-04-23 23:29:28 -0300

    Add application.properties, maven-wrapper.properties, .gitignore, AdvancedmappingsApplicationTests.java, and AdvancedmappingsApplication.java files

---
+++ src/test/java/com/luizpaulo/advancedmappings/AdvancedmappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizpaulo.advancedmappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedmappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
